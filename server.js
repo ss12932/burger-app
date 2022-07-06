c365e1b3310365e0c443123cbbaaa09ca09d9c9c
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-07-07 00:37:33 +0100

    route fail, forgot to module export router

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
-const exphbs = require('expres-handlebars');
+const exphbs = require('express-handlebars');
 
 const routes = require('./routes/handlers');
 
