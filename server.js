796265735468d7360b86dac739078028db28f703
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-07-07 00:06:28 +0100

    added mws + methodoverride mw + configure hbs

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const express = require('express');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const exphbs = require('expres-handlebars');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+
+// Configure Express Handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
