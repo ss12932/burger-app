7a52589aa07120f04f10a4965fb19000b55e90e6
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-07-07 00:29:47 +0100

    main hbs created + working on handlers route

--- routes/handlers.js
+++ routes/handlers.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const orm = require('../config/orm');
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' });
+router.get('/', function (req, res) {
+  orm.selectAll(function (error, burgers) {
+    if (error) {
+      return res.status(501).json({
+        message: 'Not able to query the database',
+      });
+    }
+  });
 });
-module.exports = router;
